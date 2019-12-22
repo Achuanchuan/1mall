ce9c7324393b2a1836b6534f7df6be439d5f7877
Achuanchuan <2517064936@qq.com>
2019-12-22 17:41:12 +0800

    mall

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  configurewebpack: {
+    resolve: {
+      alias: {
+        assets: "@/assets",
+        common: "@/common",
+        components: "@/components",
+        views: "@/views"
+      }
+    }
+  }
+};
